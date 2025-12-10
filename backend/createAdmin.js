33a7d747ff4878dba9a9e8700bc1f11f7705c559
n01711643 <n01711643@humber.ca>
2025-12-10 15:30:09 -0500

    Create Roles

---
+++ backend/createAdmin.js
@@ -0,0 +1,18 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const User = require("./modules/books/models/userModel");
+require("dotenv").config();
+
+mongoose.connect(process.env.DB_URL).then(async () => {
+  const hashed = await bcrypt.hash("123456", 10);
+
+  await User.create({
+    name: "Admin User",
+    email: "ziguruhihi@gmail.com",
+    password: hashed,
+    role: "admin"
+  });
+
+  console.log("Admin created successfully!");
+  process.exit();
+});
---
+++ backend/createUser.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const User = require("./modules/books/models/userModel");
+
+async function createUser() {
+  try {
+    const dbUrl = process.env.DB_URL;
+
+    if (!dbUrl) {
+      throw new Error(" DB_URL is undefined. Check .env file");
+    }
+
+    await mongoose.connect(dbUrl);
+    console.log("Connected to MongoDB");
+
+    const hashedPassword = await bcrypt.hash("123456", 10);
+
+    await User.create({
+      name: "Normal User",
+      email: "thienbinhhq2006@gmail.com",
+      password: hashedPassword,
+      role: "user" 
+    });
+
+    console.log("User created successfully!");
+    process.exit();
+  } catch (err) {
+    console.error("Error:", err);
+    process.exit(1);
+  }
+}
+
+createUser();
